4fd4d790874a2735ead5ac46ceb6203311f9a084
AguirreNataniel <natanielaguirre@gmail.com>
2023-11-01 13:06:07 -0400

    cuarto commit

--- src/views/AreaUpdate.vue
+++ src/views/AreaUpdate.vue
@@ -1,16 +1,16 @@
 <template>
-  <div class="container">
-      <h5>Areas Editar</h5>
-  
+    <div class="container">
+        <h5>Areas Editar</h5>
+    
 <div class="card">
-  <div class="card-content">
-      <form @submit.prevent="update()">
-          <p>Nombre: <input type="text" v-model="payload.nombre_area" required/></p>
-          <p>
-          </p>
-          <button type="submit" class="waves-effect waves-light btn-small">Editar</button>
-      </form>
-  </div>
+    <div class="card-content">
+        <form @submit.prevent="update()">
+            <p>Nombre: <input type="text" v-model="payload.nombre_area" required/></p>
+            <p>
+            </p>
+            <button type="submit" class="waves-effect waves-light btn-small">Editar</button>
+        </form>
+    </div>
 </div>
 
 </div>
@@ -22,40 +22,40 @@ name: 'AreaView',
 data() {
 const api = process.env.VUE_APP_API;
 return {
-  api,
-  items: [],
-  payload: {
-      name: null,
-  }
+    api,
+    items: [],
+    payload: {
+        name: null,
+    }
 }
 },
 methods: {
 getOne() {
-  this.axios({
-      method: 'get',
-      url: this.api + '/areas/' + this.$route.params.id
-  }).
-  then((response) => {
-      this.payload = response.data;
-  }).
-  catch((error) => {
-      console.log(error);
-  });
+    this.axios({
+        method: 'get',
+        url: this.api + '/areas/' + this.$route.params.id
+    }).
+    then((response) => {
+        this.payload = response.data;
+    }).
+    catch((error) => {
+        console.log(error);
+    });
 },
 update() {
-  if (confirm("Esta a punto de editar el área.")) {
-      this.axios({
-          method: 'patch',
-          url: this.api + '/areas/' + this.$route.params.id,
-          data: this.payload
-      }).
-      then((response) => {
-          console.log(response);
-      }).
-      catch((error) => {
-          console.log(error);
-      });
-  }
+    if (confirm("Esta a punto de editar el área.")) {
+        this.axios({
+            method: 'patch',
+            url: this.api + '/areas/' + this.$route.params.id,
+            data: this.payload
+        }).
+        then((response) => {
+            console.log(response);
+        }).
+        catch((error) => {
+            console.log(error);
+        });
+    }
 }
 },
 components: {
